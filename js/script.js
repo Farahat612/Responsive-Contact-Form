c8a6855a73e0942db4f568384923bb94abd64270
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-02-19 23:15:45 +0200

    location modal initiaization script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 new WOW().init();
+
+
+// Location Modal
+function locationModal() {
+  const locModal = document.querySelector('.location-modal');
+  const modalBtn = document.querySelector('.open-location-modal');
+  const closeBtn = document.querySelector('.close-location-modal');
+  
+  modalBtn.addEventListener('click', function() {
+    locModal.showModal();
+  });
+  
+  closeBtn.addEventListener('click', function() {
+    locModal.close();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  locationModal();
+});
